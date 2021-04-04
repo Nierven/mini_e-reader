2ec681fb7c249b640eda3f491c761914a5b8da87
Quentin <quent.rousset@gmail.com>
2021-04-04 08:04:23 +0200

    Justify algo for first par

--- e-reader/Core/Src/displayer.c
+++ e-reader/Core/Src/displayer.c
@@ -1,8 +1,12 @@
 #include <string.h>
+#include <math.h>
+
 #include "displayer.h"
 #include "ethernet.h"
 #include "toolbar.h"
 #include "ui.h"
+
+static void displayBook(void);
 
 static uint8_t visibleLayer;
 
@@ -39,36 +43,12 @@ void initScreen(void)
 
 void displayerHandler(void)
 {
-	TickType_t wakeTime = xTaskGetTickCount();
+//	TickType_t wakeTime = xTaskGetTickCount();
 
 	// Screen reset
 	BSP_LCD_Clear(backColor);
 
-	// ---------- Book display ----------
-	BSP_LCD_SetBackColor(backColor);
-	BSP_LCD_SetTextColor(textColor);
-	BSP_LCD_SetFont(textFont);
-
-	uint8_t currentLine = 0;
-	uint16_t currentCharacterIndex = 0;
-	char *currentParagraph = book;
-
-	for (int i = 0; i < bookSize; i++)
-	{
-		if (book[i] == '\0' || currentCharacterIndex == charMaxWidth)
-		{
-			BSP_LCD_DisplayStringAtLine(currentLine, (uint8_t*) currentParagraph);
-			currentParagraph = book + i + 1;
-			currentCharacterIndex = 0;
-
-			if (++currentLine == charMaxHeight)
-				break;
-		}
-
-		currentCharacterIndex++;
-	}
-
-	// ---------- Toolbar display ----------
+	displayBook();
 	drawToolbar(&mainToolbar);
 
 	// Swap the layers (double buffering)
@@ -79,4 +59,105 @@ void displayerHandler(void)
 
 	osDelay(30);
 	//osDelayUntil(&wakeTime, 34); // 30 FPS
+}
+
+static void displayLine(int start, int len, int space, int at);
+static uint32_t pos = 0;
+
+static void displayBook(void)
+{
+	BSP_LCD_SetBackColor(backColor);
+	BSP_LCD_SetTextColor(textColor);
+	BSP_LCD_SetFont(textFont);
+
+	uint8_t currentLine = 0;
+	int currentLinePos = pos;
+
+	int lettersCounter = 0;
+	int wordLettersCounter = 0;
+	int spacesCounter = 0;
+
+	const int minSpace = round(0.7 * textFont->Width);
+	const int maxSpace = round(3.0 * textFont->Width);
+
+	for (int i = pos; i < bookSize - pos; i++)
+	{
+		if (book[i] == ' ')// || book[i] == '\n')
+		{
+			// Get the optimal space length according to how many letters and space we have
+			int actualOptimalSpace;
+			if (spacesCounter != 0) actualOptimalSpace = (BSP_LCD_GetXSize() - lettersCounter * textFont->Width) / spacesCounter;
+			else actualOptimalSpace = (BSP_LCD_GetXSize() - lettersCounter * textFont->Width);
+
+			if (actualOptimalSpace < minSpace) // If too small, then discard the last word and compute the best length
+			{
+				lettersCounter -= wordLettersCounter;
+				spacesCounter--;
+
+				actualOptimalSpace = (BSP_LCD_GetXSize() - lettersCounter * textFont->Width) / spacesCounter;
+				displayLine(currentLinePos, lettersCounter + spacesCounter, actualOptimalSpace, currentLine);
+
+				currentLinePos = currentLinePos + lettersCounter + spacesCounter;
+				i = currentLinePos;
+
+				if (book[currentLinePos] == ' ') { currentLinePos++; i++; }
+				if (++currentLine == charMaxHeight)
+					break;
+
+				lettersCounter = 0;
+				wordLettersCounter = 0;
+				spacesCounter = 0;
+			}
+//			else if (book[i] == '\n')
+//			{
+//				if (actualOptimalSpace > maxSpace)
+//					actualOptimalSpace = 1;
+//
+//				displayLine(currentLinePos, i - currentLinePos, actualOptimalSpace, currentLine);
+//
+//				currentLinePos = i+1;
+//				i = currentLinePos;
+//
+//				if (++currentLine == charMaxHeight)
+//					break;
+//
+//				lettersCounter = 0;
+//				wordLettersCounter = 0;
+//				spacesCounter = 0;
+//			}
+//			else
+			{
+				spacesCounter++;
+				wordLettersCounter = 0;
+			}
+		}
+		else if (book[i] == '\n')
+		{
+			return;
+		}
+		else
+		{
+			wordLettersCounter++;
+			lettersCounter++;
+		}
+	}
+}
+
+static void displayLine(int start, int len, int space, int at)
+{
+	int current_x = 0;
+	uint8_t word[50];
+	int currentPos = start;
+
+	for (int i = start; i < start + len + 1; i++)
+	{
+		if (i == start + len || book[i] == ' ')
+		{
+			memcpy(word, book + currentPos, i - currentPos); word[i - currentPos] = '\0';
+
+			BSP_LCD_DisplayStringAt(current_x, at * textFont->Height, (uint8_t*) word, LEFT_MODE);
+			current_x += (i - currentPos) * textFont->Width + space;
+			currentPos = i+1;
+		}
+	}
 }
